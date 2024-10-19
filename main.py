b1a4a8db752f768012d3f5e10ba5958b6520ba86
elena-bilgic <elena.lipatnikova777@gmail.com>
2024-10-19 18:12:45 +0300

    added flash settings

--- main.py
+++ main.py
@@ -20,9 +20,23 @@ target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
+#Дополнительно были добавлены переменные `flash_duration` и `last_hit_time` для управления вспышкой.
+#После попадания в цель (`MOUSEBUTTONDOWN` и проверка координат) обновляем `last_hit_time`.
+#Если с момента последнего попадания прошло меньше времени, чем `flash_duration`, заполняем экран белым цветом, чтобы создать эффект вспышки.
+#Теперь, когда игрок попадает в цель, будет отображаться короткая белая вспышка.
+
+flash_duration = 100
+last_hit_time = 0
+
 running = True
 while running:
+    current_time = pygame.time.get_ticks()
     screen.fill(color)
+
+    if current_time - last_hit_time < flash_duration:
+        #отображение вспышки на экране
+        screen.fill((255, 255, 255))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -31,6 +45,8 @@ while running:
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+                last_hit_time = current_time  # Запоминаем время попадания
+
     screen.blit(target_image, (target_x, target_y))
     pygame.display.update()
 
